39abd19ae77a57e9a4275d095e97c8921701c18f
kodokami <kodokami@protonmail.com>
2026-01-12 16:56:11 +0100

    upgrading for newer python version

--- dice_roller/__init__.py
+++ dice_roller/__init__.py
@@ -1,15 +1,15 @@
 """
 dice-roller
 
-Copyright (C) 2023 _kodokami
+Copyright (C) 2023-2026 _kodokami
 """
 from .dice import Dice
 from .roller import Roller, DiceRoll
 
 __author__ = '_kodokami <kodokami@protonmail.com>'
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 
 NAME = 'dice-roller'
 COMMAND_NAME = 'roll'
 DESCRIPTION = 'Simple dice rolling program.'
-COPYRIGHT = 'Copyright (C) 2023 _kodokami'
+COPYRIGHT = 'Copyright (C) 2023-2026 _kodokami'
